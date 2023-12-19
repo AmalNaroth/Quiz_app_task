4032d2240a9624695fb33da7d47464b61fb551c2
amalnaroth <amalzenox129@gmail.com>
2023-12-19 15:39:33 +0530

    first commit -m

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Future.wait([
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]),
+  ]).then((value) {
+    runApp(
+      const MyApp(),
+    );
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: false,
+      ),
+    );
+  }
+}
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+
+  static const String homeScreen = '/home_one_screen';
+
+  static const String initialScreen = '/initial_screen';
+
+  static Map<String, WidgetBuilder> get routes => {
+        //initialScreen :
+      };
+}
